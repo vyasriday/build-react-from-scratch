66bd8621cc0d9dab8d4e434cfe16db44adbcf902
Hridayesh Sharma <dev.hridayesh@gmail.com>
2022-08-08 18:17:19 +0530

    render html elements using render function including nested elements

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # build-react-from-scratch
+
 A step by step approach to building your own react to understand how react works at core level.
+
+### Call order of (React.createElement) for a React element
+
+```jsx
+  <div className='parent'>
+      {/* createElement for deepest children is called first for a sibling to build children objects. */}
+      <p>
+        <li>Hi</li>
+        <li>there!</li>
+      </p>
+      <main>
+        <p>
+          <ul>
+            <li>Deeply</li>
+            <li>Nested</li>
+          </ul>
+        </p>
+      </main>
+      <span>Copyright 2022</span>
+      Hello World
+    </div>
+  );
+  {/*
+      * Order of createElement calls for above:
+      * called for li
+      * called for li
+      * called for p
+      * called for li
+      * called for li
+      * called for ul
+      * called for main
+      * called for span
+      * called for div -> returns final element object tree
+
+  */}
+```
+
+### Adding a React Component
--- index.tsx
+++ index.tsx
@@ -1,42 +1,54 @@
-let counter = 0;
 const React = {
   createElement: function (tag, props, ...children) {
+    if (typeof tag === 'function') {
+      return tag();
+    }
     const element = { tag, props: { ...props, children } };
-    console.log('element: ', element);
-    counter++;
     return element;
   },
 };
-const a = (
+
+const ReactDOM = {
+  render: function (reactElement, container: HTMLElement) {
+    const actualDOMElement: HTMLElement = document.createElement(
+      reactElement.tag
+    );
+
+    const {
+      props: { children, ...restProps },
+    } = reactElement;
+    console.log('children: ', children);
+    for (let key in restProps) {
+      if (['__self', '__source'].includes(key)) {
+        continue;
+      }
+      actualDOMElement[key] = restProps[key];
+    }
+
+    if (children.length === 1 && typeof children[0] === 'string') {
+      actualDOMElement.textContent = children[0];
+    }
+
+    if (children.length >= 1 && typeof children[0] === 'object') {
+      children.forEach((child) => {
+        this.render(child, actualDOMElement);
+      });
+    }
+    console.log(actualDOMElement);
+    container.appendChild(actualDOMElement);
+  },
+};
+const App = () => (
   // Finally createElement for parent is called and all the children element objects are pushed as children prop for parent
   <div className='parent'>
     {/* createElement for deepest children is called first for a sibling to build children objects. */}
-    <p>
-      <li>Hi</li>
-      <li>there!</li>
-    </p>
-    <main>
-      <p>
-        <ul>
-          <li>Deeply</li>
-          <li>Nested</li>
-        </ul>
-      </p>
-    </main>
-    <span>Copyright 2022</span>
-    Hello World
+    <p>Hello World</p>
+    <ul>
+      <li>Tomato</li>
+      <li>Potato</li>
+      <li>Brinjal</li>
+    </ul>
   </div>
 );
 
-/**
- * Order of createElement calls for above:
- * called for li
- * called for li
- * called for p
- * called for li
- * called for li
- * called for ul
- * called for main
- * called for span
- * called for div -> returns final element object tree
- */
+ReactDOM.render(<App />, document.querySelector('#app'));
